f743c9aa0c05129e5f31e2dd73add299807f7767
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-17 00:53:16 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Pipedのドメイン設定専用ファイル
+ * アイフィルターにブロックされたら、この下のドメインを書き換えるだけでOK！
+ */
+export const CONFIG = {
+    // 例: "pipedapi.kavin.rocks" や "api.piped.vicr.me" など
+    PIPED_DOMAIN: "pipedapi.kavin.rocks"
+};
