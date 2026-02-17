fc1d4960c0f853efb7ca6426e36b0e894cc48e02
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-17 11:10:12 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 window.CONFIG = {
-    YOUTUBE_API_KEY: "AIzaSyBfCvyZ_J9mJiMFNYB6WfcuLyvf9zDdcUU", // 取得したAPIキー
-    YOUTUBE_EDU_FILTER: "hmiN9tvUUI2EQM" // だいきが見つけた最強のID
+    // 自分のAPIキーを入れてくれ
+    YOUTUBE_API_KEY: "AIzaSyBfCvyZ_J9mJiMFNYB6WfcuLyvf9zDdcUU", 
+    // だいきが見つけた最強の通行証
+    YOUTUBE_EDU_FILTER: "o-hmiN9tvUUI2EQM"
 };
