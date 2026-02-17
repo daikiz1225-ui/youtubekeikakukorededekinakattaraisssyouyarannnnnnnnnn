e6a586f9c198ae7556a2183da1f7d1d3c1706a44
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-17 09:21:43 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,16 +1,24 @@
-let currentVideoId = "";
+/**
+ * 教育用ドメインを使って動画を埋め込む
+ */
+window.playVideo = function(videoId) {
+    const playerContainer = document.getElementById('player-container');
+    const viewHome = document.getElementById('view-home');
+    const viewPlayer = document.getElementById('view-player');
 
-export function launchPlayer(id) {
-    currentVideoId = id;
-    const container = document.getElementById('player-container');
-    const embedUrl = `https://www.youtubeeducation.com/embed/${id}?autoplay=1&rel=0`;
-    
-    container.innerHTML = `
-        <iframe src="${embedUrl}" width="100%" height="100%" frameborder="0" allowfullscreen allow="autoplay"></iframe>
+    // だいき指定のドメインでURLを作成
+    const videoUrl = `https://www.youtubeeducation.com/embed/${videoId}?rel=0&modestbranding=1&autoplay=1`;
+
+    playerContainer.innerHTML = `
+        <iframe 
+            src="${videoUrl}" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen
+            style="width:100%; height:100%; aspect-ratio: 16/9;">
+        </iframe>
     `;
-}
 
-// 動画を再読み込みするボタン
-document.getElementById('reload-video').addEventListener('click', () => {
-    if (currentVideoId) launchPlayer(currentVideoId);
-});
+    if (viewHome) viewHome.style.display = 'none';
+    if (viewPlayer) viewPlayer.style.display = 'block';
+};
