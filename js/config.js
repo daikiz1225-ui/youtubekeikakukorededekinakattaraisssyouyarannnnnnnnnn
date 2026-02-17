4267e79a5e5c272d6048b4c210f124ba667b544c
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-17 12:47:42 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     // 自分のAPIキーを入れてくれ
-    YOUTUBE_API_KEY: "AIzaSyCgVn-JWHKT_z6EC73Z6Vlex0F_d-BP_fY", 
+    YOUTUBE_API_KEY: "AIzaSyBbqPhAbqoWDOurTt7hejQmwc6dAoZ5Iy0", 
     // だいきが見つけた最強の通行証
     YOUTUBE_EDU_FILTER: "o-hmiN9tvUUI2EQM"
 };
