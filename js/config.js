908af1b3c8cf7c2941762a447c71177fb4a2c697
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-17 10:57:54 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
-    // ここに取得したYouTube APIキーを貼り付けてくれ
-    YOUTUBE_API_KEY: "AIzaSyBfCvyZ_J9mJiMFNYB6WfcuLyvf9zDdcUU"
+    YOUTUBE_API_KEY: "YOUR_API_KEY_HERE", // 取得したAPIキー
+    YOUTUBE_EDU_FILTER: "o-hmiN9tvUUI2EQM" // だいきが見つけた最強のID
 };
