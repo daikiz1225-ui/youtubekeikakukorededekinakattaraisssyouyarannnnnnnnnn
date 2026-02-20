2313be83b8df19dfa2a3f22b7d22b7357180ec30
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-20 11:51:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const YT = {
            "AIzaSyCgVn-JWHKT_z6EC73Z6Vlex0F_d-BP_fY",
            "AIzaSyBbqPhAbqoWDOurTt7hejQmwc6dAoZ5Iy0",
            "AIzaSyAWk9mmie23-khi8-nipv1jHJND__UtEWA", 
-           "AIzaSyBL38iyqeiaKHoKqhloSnhG590DfJ35vCE",
-           "AIzaSyDU4jrOT0o2Jd4zDwZyU5OOBsKt1P3RJNs",
-　　　　　　　"AIzaSyB2L_plk45E1wihBUB4VJ516pIfqcBc2Yw",
-　　　　　　　"AIzaSyDcYrvxFDKcXNqI65Aihrqk0uK2Ebj7KVo",
-　　　　　　　"AIzaSyAmfASO-61oyXFOfzJCR9e3oGbnKenBZbY",
-　　　　　　　"AIzaSyCU7xnDWAFbXt1ze0_DBaWDKt7NDT1XP7c",]
+           "AIzaSyBL38iyqeiaKHoKqhloSnhG590DfJ35vCE“,]
     currentEduKey: "AXH1ezlru6on842twPV_V1V5h97poSn1ucvOPWDPMYhThp-LWrcNLhBqdD4OYb3L7YrjAy8eIgJCQG1BdOWX_uYkPmS1yJ1OvYMFvsX1cK2MFku5irpgxfmaekWYXNEjododHfCKfXGA2VkUDS7UvfRK-Pb5rxEggg==",
 
     async refreshEduKey() {
