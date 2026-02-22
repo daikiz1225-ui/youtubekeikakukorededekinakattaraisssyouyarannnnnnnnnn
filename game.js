0427ff43b6b323fd3c8382f1469f44946a89904d
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-23 03:39:59 +0900

    Update game.js

--- game.js
+++ game.js
@@ -37,8 +37,8 @@ const GameModule = {
                         <h3>2048</h3>
                     </div>
 　　　　　　　　　　　　　<div class="game-card" onclick="TowerDefense.init()">
-                        <div class="game-icon">🏰</div>
-                        <h3>タワーディフェンス</h3>
+             <div class="game-icon">🏰</div>
+             <h3>タワーディフェンス</h3>
 　　　　　　　　　　　　　</div>
                 </div>
             </div>
