54b3f6009c609cbb9ee6da69da29241d26cbaec2
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-23 12:37:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const YT = {
     keys: ["AIzaSyBfCvyZ_J9mJiMFNYB6WfcuLyvf9zDdcUU", "AIzaSyCgVn-JWHKT_z6EC73Z6Vlex0F_d-BP_fY", "AIzaSyBbqPhAbqoWDOurTt7hejQmwc6dAoZ5Iy0", "AIzaSyAWk9mmie23-khi8-nipv1jHJND__UtEWA", "AIzaSyBL38iyqeiaKHoKqhloSnhG590DfJ35vCE"],
-    currentEduKey: "AXH1ezn4Dm0CUx0Ke8fEz_0pl4qZDoNyCRe5Vf4AAk9fj-zjQzR7txyZn65tHddjcAEuCLwTUddf6dtW8mIyqjwAl2WiP5THyBDdi0e1sxCbBppTVT6k6dbw33etRQg7ZW_U2_JtBC8ZfAX19ElDsUGZyMZhsoFwZg==",
+    currentEduKey: "AXH1ezmAE3vgRPcGfwKP-x8QMySX2Sc1L5ejSmbRjTuE-_q-HIR8jzGYDuaE9xpFLlo_goB3iQQBDTsJ9c0h04V6RZqjE2Le8KQULVTQBURHroB2ujwh11mxs3jKlv_VeP_HHU45QkGzad-T3gEFcKpx86UOWwnFyw==",
 
     async refreshEduKey() {
         try {
