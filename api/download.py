8b40cc3ca0c784a2d6a5a3d72ab097989633ef56
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-27 02:46:16 +0900

    Delete api/download.py

--- api/download.py
+++
@@ -1,9 +0,0 @@
-# イメージ例
-from flask import Flask, request, send_file
-import yt_dlp
-
-@app.route('/api/download')
-def download():
-    url = request.args.get('url')
-    # ここで yt-dlp を使って動画を一時保存し、send_file でユーザーに返す
-    return send_file(video_file)
