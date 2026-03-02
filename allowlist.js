76798009dc82aa51d1370aaf7e5bd9d1badf1798
daikiz1225-ui <daikiz1225@icloud.com>
2026-03-02 20:36:08 +0900

    Update allowlist.js

--- allowlist.js
+++ allowlist.js
@@ -1,7 +1,1 @@
-/* 【メンテナンス方法】
-   友達から送られてきたIDを、以下の [ ] の中にカンマ区切りで追加してください。
-   例: "DEV-ABC12345", "DEV-XYZ67890"
-*/
-const ALLOWED_IDS = [
-    "DEV-XCHJET","DEV-NHQB6M", // まずは自分のIDを後で入れる
-   ];
+const ALLOWED_IDS = ["${finalID}"];
