063be91ffdae580681d36879cd93c6ee3777274d
daikiz1225-ui <daikiz1225@icloud.com>
2026-03-25 17:52:11 +0900

    Update streaming.js

--- api/streaming.js
+++ api/streaming.js
@@ -8,7 +8,7 @@ export default async function handler(req) {
 
     // main.py と yobi.py から抽出した、今生きている可能性が高いサーバーリスト
     const APIS = [
-        'https://inv.nadeko.net',
+        'https://invidious.f5.si',
         'https://yewtu.be',
         'https://iv.nboeck.de',
         'https://invidious.perennialte.ch',
